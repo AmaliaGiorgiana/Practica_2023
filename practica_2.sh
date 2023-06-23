6084005d2bb550a836b042d939bd68fe24eabed3
AmaliaGiorgiana <125302920+AmaliaGiorgiana@users.noreply.github.com>
2023-06-23 13:14:34 +0300

    Update practica_2.sh

--- practica_2.sh
+++ practica_2.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 
-
+counter=0
+counter1=0
 while true;
  do
     who | while read utilizator terminal data ora; 
     do
-        xinput test-xi2 --root 11 --event-types 2 -l | while read eveniment; 
+        xinput test-xi2 --root 11 --event-types 2 -l -no-repeats | while read eveniment; 
         do
-        	timp=$(date +"%Y-%m-%d %H:%M:%S")
-        	cod_tasta=$(echo "$eveniment" | grep -oP '(?<=detail: ).*')
-        	caracter=$(xmodmap -e "keycode $cod_tasta" -pke | awk '{print $NF}')
-        	linie=$(echo "$timp - $utilizator - $caracter" | sed 's/[ \t]+/ /g')
-                echo "$linie" >> evenimente_tastatura.txt
+        counter1=$(($counter1+1))
+        if [[ $counter1 -gt 4 ]]
+        then
+        echo "$eveniment">> test.txt
+          if [[ counter -eq 9 ]] 
+          then
+          counter=0
+          echo "$timp - $utilizator -$cod_tasta" >>    evenimente_tastatura.txt
+          else 
+          counter=$(($counter+1))
+          fi
+        timp=$(date +"%Y-%m-%d %H:%M:%S")
+        cod_tasta=$(echo "$eveniment" | sed -n 's/.*detail: \([0-9]\+\).*/\1/p' | head -n 1)
+        fi
         done
     done
 done
