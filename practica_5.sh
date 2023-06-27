39c7bee52ba7d53e2437bf7335506fa0702dad8f
AmaliaGiorgiana <125302920+AmaliaGiorgiana@users.noreply.github.com>
2023-06-27 13:39:37 +0300

    Update practica_5.sh

--- practica_5.sh
+++ practica_5.sh
@@ -7,42 +7,50 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 # Funcție pentru a înregistra evenimentul cu marca temporală
-log_event() {
-    echo "$(date +"%Y-%m-%d %H:%M:%S"): $1"
+log_event() 
+{
+	local log_file="$1"
+	local log_line="$2"
+	local last_line="$(tail -n 1 "$log_file" 2>/dev/null)"
+	if [[ "$log_line" != "$last_line" ]]; then
+        	echo "$(date +"%Y-%m-%d %H:%M:%S"): $log_line" >> "$log_file"
+    	fi
 }
 
+log_file="activitate_retea.log"
+
 # Urmărim acțiunile utilizatorilor logați
-logged_in_users=$(who | awk '{print $1}')
+	logged_in_users=$(who | awk '{print $1}')
+while true; do
+	
+	for user in $logged_in_users; do
+    		log_event "$log_file" "Utilizator logat: $user"
 
-for user in $logged_in_users; do
-    log_event "Utilizator logat: $user"
+    	# Urmărim interogările DNS
+    	dns_queries=$(tcpdump -l -n -c 10 udp port 53 2>/dev/null | awk '{print $3}' | grep -v "0.0.0.0")
+        if [[ -n $dns_queries ]]; then
+            log_event "$log_file" "Interogări DNS efectuate de $user:"
+            for query in $dns_queries; do
+                log_event "$log_file" "  - $query"
+            done
+        fi
 
-    # Urmărim interogările DNS
-    dns_queries=$(tcpdump -l -n -c 10 udp port 53 2>/dev/null | awk '{print $3}' | grep -v "0.0.0.0")
-    if [[ -n $dns_queries ]]; then
-        log_event "Interogări DNS efectuate de $user:"
-        for query in $dns_queries; do
-            log_event "  - $query"
-        done
-    fi
+    	# Urmărim adresele IP
+    	ip_addresses=$(tcpdump -l -n -c 10 | awk '{print $3}' | grep -v "0.0.0.0")
+        if [[ -n $ip_addresses ]]; then
+            log_event "$log_file" "Adrese IP accesate de $user:"
+            for ip in $ip_addresses; do
+                log_event "$log_file" "  - $ip"
+            done
+        fi
 
-    # Urmărim adresele IP
-    ip_addresses=$(tcpdump -l -n -c 10 | awk '{print $3}' | grep -v "0.0.0.0")
-    if [[ -n $ip_addresses ]]; then
-        log_event "Adrese IP accesate de $user:"
-        for ip in $ip_addresses; do
-            log_event "  - $ip"
-        done
-    fi
-
-    # Urmărim traficul
-    traffic=$(tcpdump -l -n -c 10 2>/dev/null)
-    if [[ -n $traffic ]]; then
-        log_event "Trafic observat pentru $user:"
-        log_event "$traffic"
-    fi
+    	# Urmărim traficul
+    	traffic=$(tcpdump -l -n -c 10 2>/dev/null)
+        if [[ -n $traffic ]]; then
+            log_event "$log_file" "Trafic observat pentru $user:"
+            log_event "$log_file" "$traffic"
+        fi
+    done
+ 
 done
-
-# Exemplu de utilizare pentru a urmări activitatea utilizatorilor logați
-#./script.sh >> activitate_retea.log
 
