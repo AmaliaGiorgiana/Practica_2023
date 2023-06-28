606609c47a1ff48c97a98d71c85ab997c5ee3f52
AmaliaGiorgiana <125302920+AmaliaGiorgiana@users.noreply.github.com>
2023-06-28 13:36:15 +0300

    Update practica_1.sh

--- practica_1.sh
+++ practica_1.sh
@@ -1,31 +1,40 @@
 #!/bin/bash
 
-
 file_event()
 {
 	local eveniment="$1"
 	local fisier="$2"
+	local fisier_bun=$(echo "$fisier" | sed 's/\//_/g')
 	local user="$3"
-	local time=$(date +"%Y-%m-%d %H:%M:%S")
+	local time="$4"
+	local event_line="[$time] Utilizator: $user, Actiune: $eveniment, Fisier: $fisier_bun"
 	
-	echo "[$time] Utilizator: $user, Actiune: $eveniment, Fisier: $fisier" >> "evenimente_fisiere.txt"
+	echo $event_line
+	local var=$(grep "^$event_line$" evenimente_fisiere.txt)
+	echo "$var" >> "variabile.txt"
+	if [[ -n "$var" ]]
+	then
+		return;
+	fi
+		echo "[$time] Utilizator: $user, Actiune: $eveniment, Fisier: $fisier_bun" >> "evenimente_fisiere.txt"
 }
-
 
 monitorizare_evenimente()
 {
 	who | cut -f1 -d' ' | while read -r user
 	do
-		inotifywait -m -r -e create,delete,modify --format "%e %w" --timefmt "%Y-%m-%d %H:%M:%S" / | while read -r eveniment fisier time
+		inotifywait -m -r -e create,delete,modify --format "%e %w" /home | while read -r eveniment fisier
 		do
-			file_event "$eveniment" "$fisier" "$user"
+			time=$(date +"%Y-%m-%d %H:%M:%S")
+			echo "eveniment: $eveniment, fisier: $fisier, time: $time"
+
+       			file_event "$eveniment" "$fisier" "$user" "$time"
 		done
 	done
 			
 }
 
 monitorizare_evenimente
-
 
 
 
