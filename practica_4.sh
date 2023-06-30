1934aa72704e95f97a959e4a22ed57b7ebd5dfe4
AmaliaGiorgiana <125302920+AmaliaGiorgiana@users.noreply.github.com>
2023-06-30 08:40:23 +0300

    Update practica_4.sh

--- practica_4.sh
+++ practica_4.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
-# Obține numele utilizatorului curent
-username=$(whoami)
 start_time=$(date "+%Y-%m-%d %H:%M:%S")
-# Rulează scriptul în mod continuu
+
+
 while true; do
-    # Afiseaza evenimentele de pornire si oprire pentru utilizatorul curent
     
     journalctl -t systemd -S "$start_time" | grep -E "Starting|Stopping" | while read line; do
         timestamp=$(date "+%Y-%m-%d %H:%M:%S")
-        echo "[${timestamp}] Utilizator: ${username} - ${line}"
+        echo "[${timestamp}] Utilizator: $(whoami) - ${line}" >> "evenimente_procese.txt"
     done
 
-    # Pauză de 5 secunde între fiecare iterație
-    sleep 5
-done >> "evenimente_procese.txt"
+    sleep 2
+done 
